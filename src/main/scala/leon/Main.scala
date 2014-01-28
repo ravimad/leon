4ec9071921e9678349fbba570c2c9f8a9b5b7090
Ravichandhran Kandhadai Madhavan <kandhada@larapc12.epfl.ch>
2014-01-28 18:54:21 +0100

    Major bugfixes n elimination and debugging options

--- src/main/scala/leon/Main.scala
+++ src/main/scala/leon/Main.scala
@@ -201,7 +201,8 @@ object Main {
   def computePipeline(settings: Settings): Pipeline[List[String], Any] = {
     import purescala.Definitions.Program
 
-    val pipeBegin : Pipeline[List[String],Program] = plugin.ExtractionPhase andThen NondeterminismConverter andThen TimeStepsPhase andThen SubtypingPhase      
+    val pipeBegin : Pipeline[List[String],Program] = plugin.ExtractionPhase andThen TimeStepsPhase andThen SubtypingPhase     
+      //NondeterminismConverter andThen TimeStepsPhase andThen SubtypingPhase      
       // plugin.ExtractionPhase andThen SubtypingPhase
       //plugin.ExtractionPhase andThen TimeStepsPhase andThen SubtypingPhase
 
